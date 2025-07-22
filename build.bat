4ab24f418a6ec1e0615e3987508e87149deeef05
Gilad Sher <triplesher6@gmail.com>
2025-07-22 13:54:34 -0400

    move build scripts to root

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 @echo off
 
-mkdir ..\build
-pushd ..\build
-cd
+mkdir .\build
+pushd .\build
 cl ..\code\win32_handmade.cpp user32.lib
 popd
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,12 +1,17 @@
 #include <windows.h>
 
-int CALLBACK WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int nCmdShow
-){
-	MessageBox(0, "This is Handmade Hero.", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
-
-	return(0);
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                     LPSTR lpCmdLine, int nCmdShow) {
+  WNDCLASS WindowClass = {};
+    // UINT style;
+    // WNDPROC lpfnWndProc;
+    // int cbClsExtra;
+    // int cbWndExtra;
+    // HINSTANCE hInstance;
+    // HICON hIcon;
+    // HCURSOR hCursor;
+    // HBRUSH hbrBackground;
+    // LPCSTR lpszMenuName;
+    // LPCSTR lpszClassName;
+  return (0);
 }
