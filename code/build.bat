9031469ec1b907697186e26f81fbae077238b921
Gilad Sher <triplesher6@gmail.com>
2025-07-21 21:29:24 -0400

    add user32.lib

--- code/build.bat
+++ code/build.bat
@@ -3,5 +3,5 @@
 mkdir ..\build
 pushd ..\build
 cd
-cl ..\code\win32_handmade.cpp
+cl ..\code\win32_handmade.cpp user32.lib
 popd
--- code/debug.bat
+++ code/debug.bat
@@ -3,5 +3,5 @@
 mkdir ..\build
 pushd ..\build
 cd
-cl -Zi ..\code\win32_handmade.cpp
+cl -Zi ..\code\win32_handmade.cpp user32.lib
 popd
